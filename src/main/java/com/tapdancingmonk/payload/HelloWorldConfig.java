5270808e027f31554eb86ec7e474a0b3d5f8c5bf
J Irving <j@lollyshouse.net>
2010-03-09 01:35:52 -0500

    Walking skeleton - hello world with app engine, jersey and guice.

---
+++ src/main/java/com/tapdancingmonk/payload/HelloWorldConfig.java
@@ -0,0 +1,24 @@
+package com.tapdancingmonk.payload;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+public class HelloWorldConfig extends GuiceServletContextListener {
+
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new ServletModule() {
+
+            @Override
+            protected void configureServlets() {
+                bind(HelloWorldResource.class);
+
+                serve("/*").with(GuiceContainer.class);
+            }
+        });
+    }
+
+}
---
+++ src/main/java/com/tapdancingmonk/payload/HelloWorldResource.java
@@ -0,0 +1,18 @@
+package com.tapdancingmonk.payload;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.google.inject.servlet.RequestScoped;
+
+@Path("/helloworld")
+public class HelloWorldResource {
+
+    @RequestScoped
+    @GET
+    @Produces("text/plain")
+    public String helloWorld() {
+        return "Hello World, We Are Guiced!";
+    }
+}
